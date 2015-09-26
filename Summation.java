e7a6ae543fbacb58be6d5e23d76cc51bd9ceb1f7
gjanorg <tarzan22jones@gmail.com>
2015-09-26 10:34:38 -0700

    this is my Repo

---
+++ Summation.java
@@ -0,0 +1,14 @@
+public class Summation {
+	public static void main (String [] args){
+		int boys;
+                int girls;
+                int sum;
+		boys = 30;
+		girls = 40;
+		sum = boys * girls;
+		
+		System.out.println("THe class is " +sum);
+		
+	}
+
+}
